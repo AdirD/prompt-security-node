9b7e3ab4acd4654385a44dc42456ca434a86f531
Adir Duchan <adirdu@elementor.com>
2024-10-27 17:09:31 +0200

    fix ProtectResponseRequest typing

--- src/types/requests.ts
+++ src/types/requests.ts
@@ -4,6 +4,7 @@ export interface PromptMetadata {
   user?: string;
   userGroups?: string[];
   conversationId?: string;
+  promptResponseId?: string;
   policyName?: string;
   monitorOnly?: boolean;
   ipAddress?: string;
@@ -20,10 +21,8 @@ export interface ProtectPromptRequest extends BaseRequest {
   prompt: string;
 }
 
-export interface ProtectResponseRequest {
+export interface ProtectResponseRequest extends BaseRequest{
   response: string;
-  promptResponseId?: string;
-  metadata?: PromptMetadata;
 }
 
 export interface ProtectMultiplePromptsRequest extends BaseRequest {
